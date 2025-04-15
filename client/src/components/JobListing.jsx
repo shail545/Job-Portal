eedf915821c291e8dba68109c788a4a2a8086550
shailendra singh <141555992+shail545@users.noreply.github.com>
2025-04-15 12:00:01 +0000

    add job category filter section with checkboxes in JobListing

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -38,6 +38,26 @@ const JobListing = () => {
               </div>
             </>
           )}
+
+            {/* category filter */}
+            <div className="max-lg:hidden">
+            <h4 className="font-medium text-lg py-4">
+            Search by Categories
+            </h4>
+            <ul className="space-y-4 text-gray-600">
+              {
+                JobCategories.map((category,index)=>(
+                  <li className="flex gap-3 items-center" key={index}>
+                    <input className="scale-125" type="checkbox" name="" id="" />
+                    {category}
+                  </li>
+                ))
+              }
+            </ul>
+          </div>  
+
+
+
       </div>
     </div>
   );
