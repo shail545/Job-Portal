2967e61434bd1afac4b91bba084307aea531ecff
shailendra singh <141555992+shail545@users.noreply.github.com>
2025-04-15 12:01:24 +0000

    add job location filter section with checkboxes in JobListing

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -56,7 +56,22 @@ const JobListing = () => {
             </ul>
           </div>  
 
-
+            {/* location filter */}
+            <div className="max-lg:hidden">
+            <h4 className="font-medium text-lg py-4">
+            Search by Location
+            </h4>
+            <ul className="space-y-4 text-gray-600">
+              {
+                JobLocations.map((location,index)=>(
+                  <li className="flex gap-3 items-center" key={index}>
+                    <input className="scale-125" type="checkbox" name="" id="" />
+                    {location}
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
 
       </div>
     </div>
