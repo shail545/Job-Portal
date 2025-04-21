735b75fb3eb3101987c13058ecb1b43148c7cfae
shailendra singh <141555992+shail545@users.noreply.github.com>
2025-04-21 22:03:22 +0000

    configure application routes and recruiter login overlay

---
+++ client/src/pages/ AddJob.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddJob = () => {
+  return (
+    <div>
+      shail
+    </div>
+  )
+}
+
+export default AddJob
---
+++ client/src/pages/ViewApplications.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewApplications = () => {
+  return (
+    <div>
+      hii
+    </div>
+  )
+}
+
+export default ViewApplications
