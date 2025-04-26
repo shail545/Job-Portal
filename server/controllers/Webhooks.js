f2adfc0256c8a84f6dfbf980eac3d3ffcf039e4d
shailendra singh <141555992+shail545@users.noreply.github.com>
2025-04-26 09:15:37 +0000

    Implement Clerk webhooks handler for user events

---
+++ server/controllers/Webhooks.js
@@ -0,0 +1,59 @@
+import { Webhook } from "svix";
+import User from "../models/User.js";
+
+// API controller function to manage  clerk user with database
+const clerkWebhooks = async (req,res)=>{
+    try{
+
+        // create a Svix instance with clerk webhook  secret
+        const  whook = new Webhook (process.env.CLERK_WEBHOOK_SECRET)
+        await whook.verify(JSON.stringify(req.body),{
+            "svix-id":req ,
+            "svix-timestamp" :req.headers,
+            "svix-signature":req.headers["svix-signature"]
+        })
+
+        // getting DAta from request body 
+        const {data,type} = req.body
+
+        // switch case for diffrent events
+        switch (type) {
+            case 'user.created':{
+                const userData ={
+                    _id:data.id,
+                    email:data.email_addresses[0].email_address, 
+                    name :data.first_name + " " + data.last_name,
+                    image : data.image_url,
+                    resume : ''
+                }
+                await User.create(userData)
+                res.json({})
+                break;
+            }
+            case 'user.updated':{
+                const userData ={
+                    email:data.email_addresses[0].email_address, 
+                    name :data.first_name + " " + data.last_name,
+                    image : data.image_url,
+                }
+                await User.findByIdAndUpdate(data.id , userData)
+                res.json({})
+                break;
+            }
+
+            case 'user.deleted':{
+                await User.findByIdAndDelete(data.id)
+                res.json({})
+                break;
+            }
+            default:
+                break;
+        }
+    }catch (error){
+        console.log(error.message);
+        res.json({success:false,message:'Webhooks Error'})
+        
+
+    }
+}
+export default clerkWebhooks;
